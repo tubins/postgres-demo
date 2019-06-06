4f5a220672c915246a2241b760f97b5adc4c174d
Tubin <tubin.shaji@citrusinformatics.com>
2019-06-06 18:07:42 +0530

    Add custom Exception controller

---
+++ src/main/java/com/example/postgresdemo/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package com.example.postgresdemo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+	public ResourceNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ResourceNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
